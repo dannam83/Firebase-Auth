55388d5461390d3e3004d8dca988f2b54fc5617f
Daniel Nam <dannam83@gmail.com>
2018-11-24 16:58:51 -0500

    setup signin

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import firebase from 'firebase';
 import SignUpForm from './components/SignUpForm';
 import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      apiKey: 'AIzaSyDbERItFJ37TTIMYaZDUX0qoVaxRB8NBN8',
+      authDomain: 'one-time-password-fba7b.firebaseapp.com',
+      databaseURL: 'https://one-time-password-fba7b.firebaseio.com',
+      projectId: 'one-time-password-fba7b',
+      storageBucket: 'one-time-password-fba7b.appspot.com',
+      messagingSenderId: '50385892467'
+    };
+firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
